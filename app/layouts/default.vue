760b3e50ac0e0a6ca30f94eda599cca170c2d310
Michael Webb <therandommelonemail@gmail.com>
2025-08-24 02:24:47 -0500

    Some fixes

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
         <Header />
         <NuxtLink to="/" class="flex w-full items-center justify-center p-8 pb-0">
-            <img src="~/assets/img/ga-logo.png" class="w-1/2" />
+            <img src="~/assets/img/ga-logo.png" class="w-full lg:w-1/2 2xl:w-1/3" />
         </NuxtLink>
         <div class="container mx-auto">
             <slot />
